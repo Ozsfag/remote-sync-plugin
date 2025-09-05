a6a4b48a4c26282aaa5ed9b44c9fdae0aa12ebb2
Ozsfag <Ozsfag3154artem@gmail.com>
2025-09-05 20:36:23 +0900

    - some improvement`s

--- api-gateway/build.gradle.kts
+++ api-gateway/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     id("java")
     id("org.springframework.boot") version libs.versions.spring.boot.get()
     id("io.spring.dependency-management") version libs.versions.spring.dependency.management.get()
+    alias(libs.plugins.jacoco)
 }
 
 group = "org.blacksoil.apigateway"
@@ -31,3 +32,40 @@ dependencyManagement {
         mavenBom("org.springframework.cloud:spring-cloud-dependencies:${libs.versions.spring.cloud.get()}")
     }
 }
+
+jacoco {
+    toolVersion = libs.versions.jacoco.get()
+}
+
+tasks.test {
+    useJUnitPlatform()
+    finalizedBy(tasks.jacocoTestReport)
+}
+
+tasks.jacocoTestReport {
+    dependsOn(tasks.test)
+
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+}
+
+tasks.jacocoTestCoverageVerification {
+    dependsOn(tasks.test)
+
+    violationRules {
+        rule {
+            limit {
+                counter = "INSTRUCTION"
+                value = "COVEREDRATIO"
+                minimum = "0.80".toBigDecimal()
+            }
+        }
+    }
+}
+
+tasks.check {
+    dependsOn(tasks.jacocoTestCoverageVerification)
+}
+
