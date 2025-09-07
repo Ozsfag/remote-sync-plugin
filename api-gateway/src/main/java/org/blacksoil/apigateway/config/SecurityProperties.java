eb1adee42c95dd904eb6dbe8be2f412f2977b183
Ozsfag <Ozsfag3154artem@gmail.com>
2025-09-07 21:15:00 +0900

    -_-

--- api-gateway/src/main/java/org/blacksoil/apigateway/config/SecurityProperties.java
+++ api-gateway/src/main/java/org/blacksoil/apigateway/config/SecurityProperties.java
@@ -2,8 +2,6 @@ package org.blacksoil.apigateway.config;
 
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ConfigurationProperties(prefix = "security")
 public record SecurityProperties(List<String> permittedPaths) {}
