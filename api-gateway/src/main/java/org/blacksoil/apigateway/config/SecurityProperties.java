7f0513282767d4f3909986a5aac6fbb51ffca765
Ozsfag <Ozsfag3154artem@gmail.com>
2025-09-07 13:04:38 +0900

    - refactor SecurityProperties

--- api-gateway/src/main/java/org/blacksoil/apigateway/config/SecurityProperties.java
+++ api-gateway/src/main/java/org/blacksoil/apigateway/config/SecurityProperties.java
@@ -2,6 +2,8 @@ package org.blacksoil.apigateway.config;
 
 import java.util.List;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 @ConfigurationProperties(prefix = "security")
 public record SecurityProperties(List<String> permittedPaths) {}
